55bdfa5dea523bc940ab91b08b4860ecda7d4f60
Scott Hoyt <scott@wildonion.io>
2015-09-03 10:56:07 -0700

    Initial Commit

---
+++ Optimize/Optimize.h
@@ -0,0 +1,19 @@
+//
+//  Optimize.h
+//  Optimize
+//
+//  Created by Scott Hoyt on 9/3/15.
+//  Copyright © 2015 Scott Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Optimize.
+FOUNDATION_EXPORT double OptimizeVersionNumber;
+
+//! Project version string for Optimize.
+FOUNDATION_EXPORT const unsigned char OptimizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Optimize/PublicHeader.h>
+
+
