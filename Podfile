fccc3003db3aad4b4f00893a6666312912a45c79
Scott Hoyt <scott@wildonion.io>
2015-09-03 20:39:39 -0700

    Added Surge

---
+++ Podfile
@@ -0,0 +1,4 @@
+use_frameworks!
+
+pod 'Surge', :git => 'https://github.com/scottrhoyt/Surge.git', :branch => 'Swift2'
+
---
+++ Pods/Target Support Files/Surge/Surge-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SurgeVersionNumber;
+FOUNDATION_EXPORT const unsigned char SurgeVersionString[];
+
